b864b95b678c5c79f8f28a1cb9fd89981ad6a626
dungahk <emersonjairr@gmail.com>
2018-10-24 17:24:28 -0300

    Initial package structure

---
+++ src/UnleashClient.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Unleash;
+
+class UnleashClient
+{
+    public function initialize($options)
+    {
+        throw new \Exception("Not implemented", 1);
+
+    }
+
+    public function isEnabled($name, $context, $fallbackValue)
+    {
+        throw new \Exception("Not implemented", 1);
+
+    }
+
+    public function destroy()
+    {
+        throw new \Exception("Not implemented", 1);
+
+    }
+
+    public function getFeatureToggleDefinition($toggleName)
+    {
+        throw new \Exception("Not implemented", 1);
+
+    }
+
+    public function count($toggleName, $enabled)
+    {
+        throw new \Exception("Not implemented", 1);
+
+    }
+}
