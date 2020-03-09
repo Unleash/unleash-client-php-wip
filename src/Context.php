93b52ebf504216da8e7e623676a6566ac03bbbf3
Kay Strobach <info@kay-strobach.de>
2020-03-09 08:07:28 +0100

    [TASK] allow context to be initialized from an array

--- src/Context.php
+++ src/Context.php
@@ -8,4 +8,13 @@ class Context
     public $sessionId;
     public $remoteAddress;
     public $properties;
+
+    public function __construct(array $defaults = [])
+    {
+        foreach ($defaults as $key => $value) {
+            if (property_exists($this, $key)) {
+                $this->$key = $value;
+            }
+        }
+    }
 }
